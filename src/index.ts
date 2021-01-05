6878ad26b29c4bbcde851515bbea5e9617866cac
Leo Ding Hao <leo.ding.hao.2014@gmail.com>
2021-01-05 22:19:59 +0800

    added maxage for static content to prevent refetching

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ app.use(requestLogger);
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use('/', router);
-app.use('/tmp', express.static('tmp'));
+app.use('/tmp', express.static('tmp', { maxAge: 3600000 }));
 app.use(errorLogger);
 app.use(errorHandler);
 
